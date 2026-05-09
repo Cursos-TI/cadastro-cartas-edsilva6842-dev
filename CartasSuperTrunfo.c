072c20baf672d1d9a32a5ff8dc076bb5540af23d
Eduardo Cruz <283043434+edsilva6842-dev@users.noreply.github.com>
2026-05-09 23:31:50 +0000

    Fiz o desafio nivel 1, que consiste em criar um programa em C para cadastrar cartas de um jogo de Super Trunfo. O programa solicita ao usuário informações sobre duas cartas, como estado, código da carta, cidade, população, área, PIB e número de pontos turísticos. Após o cadastro, o programa exibe os dados cadastrados para ambas as cartas, fiz auuterações no código, como adicionar informações pré-definidas para as cartas, como população, área, PIB e número de pontos turísticos, para tornar o programa mais completo e informativo. O programa agora exibe essas informações junto com os dados cadastrados pelo usuário.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,71 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main(void){
+  char estado1[10];
+  char codigo_da_carta1[10];
+  char Cidade1[10];
+  int populacao1;
+  float area1, pib1;
+  char nomes_pontos_turisticos1[100];
+  int num_pontos_turisticos1;
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado2[10];
+  char codigo_da_carta2[10];
+  char Cidade2[10];
+  int populacao2;
+  float area2, pib2;
+  char nomes_pontos_turisticos2[100];
+  int num_pontos_turisticos2;
 
-  // Área para entrada de dados
+  printf(" CARTA 1 \n");
+  printf("Estado: \n");
+  scanf("%9s", estado1);
 
-  // Área para exibição dos dados da cidade
+  printf("Codigo da carta: \n");
+  scanf("%9s", codigo_da_carta1);
 
-return 0;
-} 
+  printf("Cidade: \n");
+  scanf("%9s", Cidade1);
+
+  printf("Populacao:13.000.000 \n");
+  scanf("%d", &populacao1);
+
+  printf("Area (km2):1.521 \n");
+  scanf("%f", &area1);
+
+  printf("PIB:3.000.000.000.000 \n");
+  scanf("%f", &pib1);
+
+  printf("Numero de pontos turisticos:158 Museus, 30 teatro/casas de shows \n");
+  scanf("%d", &num_pontos_turisticos1);
+
+  printf(" CARTA 2 \n");
+  printf("Estado: B \n");
+  scanf("%9s", estado2);
+
+  printf("Codigo da carta: \n");
+  scanf("%9s", codigo_da_carta2);
+
+  printf("cidade: \n");
+  scanf("%9s", Cidade2);
+
+  printf("Populacao:6.000.000 \n");
+  scanf("%d", &populacao2);
+
+  printf("Area (km2):1.200 \n");
+  scanf("%f", &area2);
+
+  printf("PIB:359.000.000.000 \n");
+  scanf("%f", &pib2);
+
+  printf("Numero de pontos turisticos:40 \n");
+  scanf("%d", &num_pontos_turisticos2);
+
+  printf("\n=== DADOS CADASTRADOS ===\n");
+  printf("Carta 1: %s %s %s | Pop:13.000.000 %d | Area:1.521 %.3f |  PIB:3.000.000.000.000 %.0f\n",
+         estado1, codigo_da_carta1, Cidade1, populacao1, area1, pib1);
+  printf("Carta 2: %s %s %s | Pop:6.000.000 %d | Area:1.200  %.3f | PIB:359.000.000.000 %.0f\n",
+         estado2, codigo_da_carta2, Cidade2, populacao2, area2, pib2);
+
+  return 0;
+}
